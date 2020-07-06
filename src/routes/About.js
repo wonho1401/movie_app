9962815f51e0976240a106aa09e13aaa357bcbf9
wonho1401 <wonho1401@naver.com>
2020-07-06 14:50:48 +0900

    Add Navigator and fix CSS

---
+++ src/routes/About.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./About.css";
+function About() {
+  return (
+    <div className="about__container">
+      <span>Study React.</span>
+      <span>− Wonho , 2020 -</span>
+    </div>
+  );
+}
+
+export default About;
